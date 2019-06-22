bd9b95080070c619b3a3160ea6e4526721689fd0
Unknown <programmater@gmail.com>
2019-06-22 20:57:26 +1000

    РОУТЫ

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/grisha', function () {
+    return view('grisha');
+});
+
+
+Route::get('/egor', function () {
+    return view('egor');
+});
